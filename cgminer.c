f4a8edb93b244e6b74e08ec4125a0e1bffce48d7
Mikeqin <Fengling.Qin@gmail.com>
2016-08-04 19:41:13 +0800

    Add Avalon Miner 7xx support

--- cgminer.c
+++ cgminer.c
@@ -81,6 +81,10 @@ char *curly = ":D";
 #include "driver-avalon4.h"
 #endif
 
+#ifdef USE_AVALON7
+#include "driver-avalon7.h"
+#endif
+
 #ifdef USE_AVALON_MINER
 #include "driver-avalon-miner.h"
 #endif
@@ -247,6 +251,11 @@ static char *opt_set_avalon2_voltage;
 static char *opt_set_avalon4_fan;
 static char *opt_set_avalon4_voltage;
 static char *opt_set_avalon4_freq;
+#endif
+#ifdef USE_AVALON7
+static char *opt_set_avalon7_fan;
+static char *opt_set_avalon7_voltage;
+static char *opt_set_avalon7_freq;
 #endif
 #ifdef USE_AVALON_MINER
 static char *opt_set_avalonm_voltage;
@@ -1319,6 +1328,53 @@ static struct opt_table opt_config_table[] = {
 		     opt_set_intval, opt_show_intval, &opt_avalon4_freqadj_temp,
 		     "Set Avalon4 check temperature when run into AVA4_FREQ_TEMPADJ_MODE"),
 #endif
+#ifdef USE_AVALON7
+	OPT_WITH_CBARG("--avalon7-voltage",
+		     set_avalon7_voltage, NULL, &opt_set_avalon7_voltage,
+		     "Set Avalon7 default core voltage, in millivolts, step: 125"),
+	OPT_WITH_CBARG("--avalon7-freq",
+		     set_avalon7_freq, NULL, &opt_set_avalon7_freq,
+		     "Set Avalon7 default frequency, range:[100, 500], step: 25, example: 500"),
+	OPT_WITH_CBARG("--avalon7-fan",
+		     set_avalon7_fan, NULL, &opt_set_avalon7_fan,
+		     "Set Avalon7 target fan speed, range:[0, 100], step: 1, example: 0-100"),
+	OPT_WITH_ARG("--avalon7-temp",
+		     set_int_22_to_75, opt_show_intval, &opt_avalon7_temp_target,
+		     "Set Avalon7 target temperature"),
+	OPT_WITH_ARG("--avalon7-cutoff",
+		     set_int_42_to_85, opt_show_intval, &opt_avalon7_overheat,
+		     "Set Avalon7 overheat cut off temperature"),
+	OPT_WITH_ARG("--avalon7-polling-delay",
+		     set_int_1_to_65535, opt_show_intval, &opt_avalon7_polling_delay,
+		     "Set Avalon7 polling delay value (ms)"),
+	OPT_WITH_ARG("--avalon7-ntime-offset",
+		     opt_set_intval, opt_show_intval, &opt_avalon7_ntime_offset,
+		     "Set Avalon7 MM ntime rolling max offset"),
+	OPT_WITH_ARG("--avalon7-aucspeed",
+		     opt_set_intval, opt_show_intval, &opt_avalon7_aucspeed,
+		     "Set Avalon7 AUC IIC bus speed"),
+	OPT_WITH_ARG("--avalon7-aucxdelay",
+		     opt_set_intval, opt_show_intval, &opt_avalon7_aucxdelay,
+		     "Set Avalon7 AUC IIC xfer read delay, 4800 ~= 1ms"),
+	OPT_WITH_ARG("--avalon7-smart-speed",
+		     opt_set_intval, opt_show_intval, &opt_avalon7_smart_speed,
+		     "Set Avalon7 smart speed, range 0-3. 0 means Disable"),
+	OPT_WITHOUT_ARG("--avalon7-iic-detect",
+		     opt_set_bool, &opt_avalon7_iic_detect,
+		     "Enable Avalon7 detect through iic controller"),
+	OPT_WITH_ARG("--avalon7-freqadj-time",
+		     set_int_1_to_60, opt_show_intval, &opt_avalon7_freqadj_time,
+		     "Set Avalon7 check interval when run in AVA4_FREQ_TEMPADJ_MODE"),
+	OPT_WITH_ARG("--avalon7-delta-temp",
+		     opt_set_intval, opt_show_intval, &opt_avalon7_delta_temp,
+		     "Set Avalon7 delta temperature when reset freq in AVA4_FREQ_TEMPADJ_MODE"),
+	OPT_WITH_ARG("--avalon7-delta-freq",
+		     opt_set_intval, opt_show_intval, &opt_avalon7_delta_freq,
+		     "Set Avalon7 delta freq when adjust freq in AVA4_FREQ_TEMPADJ_MODE"),
+	OPT_WITH_ARG("--avalon7-freqadj-temp",
+		     opt_set_intval, opt_show_intval, &opt_avalon7_freqadj_temp,
+		     "Set Avalon7 check temperature when run into AVA4_FREQ_TEMPADJ_MODE"),
+#endif
 #ifdef USE_AVALON_MINER
 	OPT_WITH_CBARG("--avalonm-voltage",
 		     set_avalonm_voltage, NULL, &opt_set_avalonm_voltage,
@@ -1960,6 +2016,9 @@ static char *opt_verusage_and_exit(const char *extra)
 #endif
 #ifdef USE_AVALON4
 		"avalon4 "
+#endif
+#ifdef USE_AVALON7
+		"avalon7 "
 #endif
 #ifdef USE_AVALON_MINER
 		"avalon miner"
@@ -6871,7 +6930,7 @@ void set_target(unsigned char *dest_target, double diff)
 	cg_memcpy(dest_target, target, 32);
 }
 
-#if defined (USE_AVALON2) || defined (USE_AVALON4) || defined (USE_AVALON_MINER) || defined (USE_HASHRATIO)
+#if defined (USE_AVALON2) || defined (USE_AVALON4) || defined (USE_AVALON7) || defined (USE_AVALON_MINER) || defined (USE_HASHRATIO)
 bool submit_nonce2_nonce(struct thr_info *thr, struct pool *pool, struct pool *real_pool,
 			 uint32_t nonce2, uint32_t nonce,  uint32_t ntime)
 {
---
+++ driver-avalon7.h
@@ -0,0 +1,251 @@
+/*
+ * Copyright 2016 Mikeqin <Fengling.Qin@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option)
+ * any later version.  See COPYING for more details.
+ */
+
+#ifndef _AVALON7_H_
+#define _AVALON7_H_
+
+#include "util.h"
+#include "i2c-context.h"
+
+#ifdef USE_AVALON7
+
+#define AVA7_DEFAULT_FAN_MIN	10 /* % */
+#define AVA7_DEFAULT_FAN_MAX	100
+
+#define AVA7_DEFAULT_TEMP_TARGET	65
+#define AVA7_DEFAULT_TEMP_OVERHEAT	85
+#define AVA7_MM701_TEMP_FREQADJ	70
+
+#define AVA7_DEFAULT_VOLTAGE_MIN	4000
+#define AVA7_DEFAULT_VOLTAGE_MAX	9000
+
+#define AVA7_DEFAULT_FREQUENCY_MIN	100
+#define AVA7_DEFAULT_FREQUENCY_MAX	500
+#define AVA7_MM701_FREQUENCY_MAX	500
+
+#define AVA7_DEFAULT_MODULARS	7	/* Only support 6 modules maximum with one AUC */
+#define AVA7_DEFAULT_MINER_MAX	8
+#define AVA7_DEFAULT_ASIC_MAX	40
+
+#define AVA7_MM701_MINER_CNT	4
+#define AVA7_MM701_ASIC_CNT	14
+
+#define AVA7_DEFAULT_POLLING_DELAY	20 /* ms */
+#define AVA7_DEFAULT_NTIME_OFFSET 4
+
+#define AVA7_DEFAULT_SMARTSPEED_OFF 0
+#define AVA7_DEFAULT_SMARTSPEED_MODE1 1
+#define AVA7_DEFAULT_SMARTSPEED_MODE2 2
+#define AVA7_DEFAULT_SMART_SPEED	(AVA7_DEFAULT_SMARTSPEED_MODE2)
+
+#define AVA7_DEFAULT_IIC_DETECT	false
+
+#define AVA7_PWM_MAX	0x3FF
+#define AVA7_DRV_DIFFMAX	1024
+
+#define AVA7_MODULE_DETECT_INTERVAL	30 /* 30 s */
+
+#define AVA7_AUC_VER_LEN	12	/* Version length: 12 (AUC-YYYYMMDD) */
+#define AVA7_AUC_SPEED		400000
+#define AVA7_AUC_XDELAY  	19200	/* 4800 = 1ms in AUC (11U14)  */
+#define AVA7_AUC_P_SIZE		64
+
+#define AVA7_CONNECTER_AUC	1
+#define AVA7_CONNECTER_IIC	2
+
+/* avalon7 protocol package type from MM protocol.h
+ * https://github.com/Canaan-Creative/MM/blob/avalon7/firmware/protocol.h */
+#define AVA7_MM_VER_LEN	15
+#define AVA7_MM_DNA_LEN	8
+#define AVA7_H1	'C'
+#define AVA7_H2	'N'
+
+#define AVA7_P_COINBASE_SIZE	(6 * 1024 + 64)
+#define AVA7_P_MERKLES_COUNT	30
+
+#define AVA7_P_COUNT	40
+#define AVA7_P_DATA_LEN 32
+
+/* Broadcase with block iic_write*/
+#define AVA7_P_DETECT	0x10
+
+/* Broadcase With non-block iic_write*/
+#define AVA7_P_STATIC	0x11
+#define AVA7_P_JOB_ID	0x12
+#define AVA7_P_COINBASE	0x13
+#define AVA7_P_MERKLES	0x14
+#define AVA7_P_HEADER	0x15
+#define AVA7_P_TARGET	0x16
+
+/* Broadcase or Address */
+#define AVA7_P_SET	0x20
+#define AVA7_P_FINISH	0x21
+#define AVA7_P_SET_VOLT 0x22
+#define AVA7_P_SET_FREQ 0x23
+
+/* Have to with I2C address */
+#define AVA7_P_POLLING	0x30
+#define AVA7_P_REQUIRE	0x31
+#define AVA7_P_TEST	0x32
+#define AVA7_P_RSTMMTX	0x33
+#define AVA7_P_GET_VOLT 0x34
+
+/* Back to host */
+#define AVA7_P_ACKDETECT	0x40
+#define AVA7_P_STATUS		0x41
+#define AVA7_P_NONCE		0x42
+#define AVA7_P_TEST_RET		0x43
+#define AVA7_P_STATUS_LW        0x44
+#define AVA7_P_STATUS_HW        0x45
+#define AVA7_P_STATUS_VOLT	0x46
+#define AVA7_P_STATUS_MA	0x47
+#define AVA7_P_STATUS_M	0x48
+
+#define AVA7_MODULE_BROADCAST	0
+/* Endof avalon7 protocol package type */
+
+#define AVA7_MM701_PREFIXSTR	"701"
+#define AVA7_MM_VERNULL		"NONE"
+
+#define AVA7_TYPE_MM701		701
+#define AVA7_TYPE_NULL		00
+
+#define AVA7_IIC_RESET		0xa0
+#define AVA7_IIC_INIT		0xa1
+#define AVA7_IIC_DEINIT		0xa2
+#define AVA7_IIC_XFER		0xa5
+#define AVA7_IIC_INFO		0xa6
+
+#define AVA7_FREQ_INIT_MODE	0x0
+#define AVA7_FREQ_CUTOFF_MODE	0x1
+#define AVA7_FREQ_TEMPADJ_MODE	0x2
+#define AVA7_FREQ_PLLADJ_MODE	0x3
+
+#define AVA7_DEFAULT_FAVG_TIME	(15 * 60.0)
+#define AVA7_DEFAULT_FREQADJ_TIME	60
+
+#define AVA7_DEFAULT_DELTA_T	0
+#define AVA7_DEFAULT_DELTA_FREQ	100
+
+struct avalon7_pkg {
+	uint8_t head[2];
+	uint8_t type;
+	uint8_t opt;
+	uint8_t idx;
+	uint8_t cnt;
+	uint8_t data[32];
+	uint8_t crc[2];
+};
+#define avalon7_ret avalon7_pkg
+
+struct avalon7_info {
+	/* Public data */
+	uint32_t newnonce;
+
+	int polling_first;
+	int mm_count;
+	int xfer_err_cnt;
+	int pool_no;
+
+	struct timeval firsthash;
+	struct timeval last_fan_adj;
+	struct timeval last_stratum;
+	struct timeval last_detect;
+	struct timeval last_fadj;
+	struct timeval last_tcheck;
+
+	cglock_t update_lock;
+
+	struct pool pool0;
+	struct pool pool1;
+	struct pool pool2;
+
+	/* For connecter */
+	char auc_version[AVA7_AUC_VER_LEN + 1];
+
+	int auc_speed;
+	int auc_xdelay;
+	int auc_sensor;
+
+	struct i2c_ctx *i2c_slaves[AVA7_DEFAULT_MODULARS];
+
+	uint8_t connecter; /* AUC or IIC */
+
+	/* For modulars */
+	bool enable[AVA7_DEFAULT_MODULARS];
+
+	struct timeval elapsed[AVA7_DEFAULT_MODULARS];
+	struct timeval last_favg[AVA7_DEFAULT_MODULARS];
+
+	uint8_t mm_dna[AVA7_DEFAULT_MODULARS][AVA7_MM_DNA_LEN + 1];
+	char mm_version[AVA7_DEFAULT_MODULARS][AVA7_MM_VER_LEN + 1];
+	uint32_t total_asics[AVA7_DEFAULT_MODULARS];
+
+	int mod_type[AVA7_DEFAULT_MODULARS];
+	uint8_t miner_count[AVA7_DEFAULT_MODULARS];
+	uint8_t asic_count[AVA7_DEFAULT_MODULARS];
+	int toverheat[AVA7_DEFAULT_MODULARS];
+	int temp_target[AVA7_DEFAULT_MODULARS];
+	int fan_pct[AVA7_DEFAULT_MODULARS];
+
+	uint32_t set_voltage[AVA7_DEFAULT_MODULARS][AVA7_DEFAULT_MINER_MAX];
+
+	uint32_t freq_mode[AVA7_DEFAULT_MODULARS];
+	unsigned int set_frequency[AVA7_DEFAULT_MODULARS][AVA7_DEFAULT_MINER_MAX];
+
+	int led_red[AVA7_DEFAULT_MODULARS];
+	uint8_t cutoff[AVA7_DEFAULT_MODULARS];
+
+	int get_voltage[AVA7_DEFAULT_MODULARS];
+	int get_frequency[AVA7_DEFAULT_MODULARS];
+	int error_code[AVA7_DEFAULT_MODULARS];
+	int fan_cpm[AVA7_DEFAULT_MODULARS];
+	int temp[AVA7_DEFAULT_MODULARS];
+	int last_maxtemp[AVA7_DEFAULT_MODULARS];
+
+	uint64_t local_works[AVA7_DEFAULT_MODULARS];
+	uint64_t local_works_i[AVA7_DEFAULT_MODULARS][AVA7_DEFAULT_MINER_MAX];
+	uint64_t hw_works[AVA7_DEFAULT_MODULARS];
+	uint64_t hw_works_i[AVA7_DEFAULT_MODULARS][AVA7_DEFAULT_MINER_MAX];
+	int chipmatching_work[AVA7_DEFAULT_MODULARS][AVA7_DEFAULT_MINER_MAX][AVA7_DEFAULT_ASIC_MAX];
+
+	int polling_err_cnt[AVA7_DEFAULT_MODULARS];
+};
+
+struct avalon7_iic_info {
+	uint8_t iic_op;
+	union {
+		uint32_t aucParam[2];
+		uint8_t slave_addr;
+	} iic_param;
+};
+
+#define AVA7_WRITE_SIZE (sizeof(struct avalon7_pkg))
+#define AVA7_READ_SIZE AVA7_WRITE_SIZE
+
+#define AVA7_SEND_OK 0
+#define AVA7_SEND_ERROR -1
+
+extern char *set_avalon7_fan(char *arg);
+extern char *set_avalon7_freq(char *arg);
+extern char *set_avalon7_voltage(char *arg);
+extern int opt_avalon7_temp_target;
+extern int opt_avalon7_overheat;
+extern int opt_avalon7_polling_delay;
+extern int opt_avalon7_aucspeed;
+extern int opt_avalon7_aucxdelay;
+extern int opt_avalon7_ntime_offset;
+extern int opt_avalon7_smart_speed;
+extern bool opt_avalon7_iic_detect;
+extern int opt_avalon7_freqadj_time;
+extern int opt_avalon7_delta_temp;
+extern int opt_avalon7_delta_freq;
+extern int opt_avalon7_freqadj_temp;
+#endif /* USE_AVALON7 */
+#endif	/* _AVALON7_H_ */
