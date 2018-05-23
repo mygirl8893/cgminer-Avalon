dfd57b06a77e58460b24cc4a55891a4d9a772157
Johnson-Fan <1314zhengyi@gmail.com>
2018-05-23 15:58:30 +0800

    Update AVA8_MM851_VOUT_ADC_RATIO

--- driver-avalon8.h
+++ driver-avalon8.h
@@ -177,7 +177,7 @@
 
 #define AVA8_MM821_VOUT_ADC_RATIO	(3.3 / 4095.0 * 63.0 / 20.0 * 10000.0 * 100.0)
 #define AVA8_MM841_VOUT_ADC_RATIO	(3.3 / 4095.0 * 63.0 / 20.0 * 10000.0 * 100.0)
-#define AVA8_MM851_VOUT_ADC_RATIO	(3.3 / 4095.0 * 63.0 / 20.0 * 10000.0 * 100.0)
+#define AVA8_MM851_VOUT_ADC_RATIO	(3.3 / 4095.0 * 72.3 / 20.0 * 10000.0 * 100.0)
 
 struct avalon8_pkg {
 	uint8_t head[2];
